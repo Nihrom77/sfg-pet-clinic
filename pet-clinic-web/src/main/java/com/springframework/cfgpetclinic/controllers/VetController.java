2ffb32bde5cac3c37948a914684d8bafb9bcc9bb
Nihrom77 <Snoll@yandex.ru>
2020-01-26 21:06:35 +0300

    Вывел список на /vets модели из vets. Closes #12

--- pet-clinic-web/src/main/java/com/springframework/cfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/cfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.springframework.cfgpetclinic.controllers;
 
+import com.springframework.cfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
