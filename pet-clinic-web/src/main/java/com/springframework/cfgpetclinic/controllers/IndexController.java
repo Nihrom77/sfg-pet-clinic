7c2d4b5fc876e8b4fcefc371eb7a67ad3cc8b2a3
Nihrom77 <Snoll@yandex.ru>
2020-02-01 23:55:10 +0300

    Добавил ресурсы less и плагин для  владельцев и ветеринаров.

--- pet-clinic-web/src/main/java/com/springframework/cfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/cfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.springframework.cfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,8 @@ public class IndexController {
 
     //Если запрос заканчивается на любое из этих имен, он будет обработан этим контроллером
     @RequestMapping({"", "/", "index", "index.html"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("welcome", "Welcome");
         return "index";
     }
 }
