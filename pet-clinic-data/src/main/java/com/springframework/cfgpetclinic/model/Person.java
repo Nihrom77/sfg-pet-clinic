b035f2f94425ef95981658039190f5b7a86beda1
Nihrom77 <Snoll@yandex.ru>
2020-02-07 17:01:35 +0300

    Update to JPA

--- pet-clinic-data/src/main/java/com/springframework/cfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springframework/cfgpetclinic/model/Person.java
@@ -1,7 +1,14 @@
 package com.springframework.cfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //Когда от этого класс наследуются другие классы
 public class Person extends BaseEntity{
+    @Column(name = "first_name") //По умолчанию было бы first_name (snake case)
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
