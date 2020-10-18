8f8a48045bef9a79583c0be93ac8e12cbd281148
NikaMars <nikangeloflight@gmail.com>
2020-10-18 20:39:39 +0300

    Добавил простой запуск тестов.

---
+++ pet-clinic-web/src/test/java/com/springframework/cfgpetclinic/CfgpetclinicApplicationTest.java
@@ -0,0 +1,19 @@
+package com.springframework.cfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CfgpetclinicApplicationTest {
+
+//    @Autowired
+//    IndexController indexController;
+
+    @Test
+    public void test() {
+
+    }
+}
